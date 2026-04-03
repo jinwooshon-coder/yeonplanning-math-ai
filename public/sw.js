addadc9b0c1786d45975641533a8264ccf866aeb
연소사 <jinwooshon@gmail.com>
2026-04-03 18:29:23 +0900

    진도/평가/문제뱅크 탭 구현

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'supulai-cache-v9';
+const CACHE_NAME = 'supulai-cache-v10';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
@@ -37,17 +37,4 @@ self.addEventListener('fetch', (e) => {
                               e.respondWith(
                                       fetch(e.request)
                                         .then((res) => {
-                                                    const clone = res.clone();
-                                                    caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone));
-                                                    return res;
-                                        })
-                                        .catch(() => caches.match(e.request))
-                                    );
-                              return;
-                        }
-
-                        // 나머지 정적 자원은 캐시 우선
-                        e.respondWith(
-                              caches.match(e.request).then((cached) => cached || fetch(e.request))
-                            );
-});
+                                   
